99c0b4bf2973033d3fac7cb359659c6b66a0ebdd
Mishinia <ishuckn5@gmail.com>
2023-02-10 14:00:55 -0800

    Update bubble_sort.rs

--- src/bubble_sort.rs
+++ src/bubble_sort.rs
@@ -1,1 +1,12 @@
 
+fn bubble_sort(arr: &mut [i32]) {
+    let n = arr.len();
+
+    for i in 0..n {
+        for j in 0..n - i - 1 {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
